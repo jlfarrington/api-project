4477b6933adba8e3ab359ce22508cc3af48575eb
Jenn Farrington <jlfarrington0@gmail.com>
2020-12-10 19:39:41 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let baseURL = "https://picsum.photos/900/400"
+
+const banText = document.querySelector('#ban-text');
+const txtColor = document.querySelector('#color');
+const blurCheck = document.querySelector('#blur');
+const grayscaleCheck = document.querySelector('#grayscale');
+const form = document.querySelector('form');
+
+
+form.addEventListener("submit", genBanner);
+
+// function genBanner(e) {
+//         e.preventDefault();
+//         if (grayscaleCheck.checked === true && blurCheck.checked === true){
+//             baseURL += '?grayscale?blur=5';
+//         } else if (grayscaleCheck.checked === true){
+//          baseURL += '?grayscale';
+//         } else if (blurCheck.checked === true){
+//             baseURL += '?blur=10';
+//         }
+//         fetch(baseURL)
+//             .then(json => displayResults(json))
+// }
+
+async function genBanner(e) {
+    e.preventDefault();
+    if (grayscaleCheck.checked === true && blurCheck.checked === true){
+        baseURL += '?grayscale?blur=5';
+    } else if (grayscaleCheck.checked === true){
+     baseURL += '?grayscale';
+    } else if (blurCheck.checked === true){
+        baseURL += '?blur=10';
+    }
+    let fetch_randy = await fetch(baseURL)
+            .then(json => displayResults(json))
+    
+    //form.reset();
+    //console.log("fuck you");
+}
+
+
+function displayResults(json) {
+    let img = json.url;
+    let section = document.querySelector('.ban');
+    while (section.firstChild){
+        section.removeChild(section.firstChild);
+    }
+    let banner = document.createElement('img')
+    banner.src = img;
+    section.appendChild(banner);
+}
